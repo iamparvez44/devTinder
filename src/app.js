64327067087ff3f49f5b1951b2f3ff1523fb24bb
parvez alam <parvez.alam44@gmail.com>
2026-08-14 16:36:00 +0530

    make understanding about data sanatization, hashpassword and other small things

--- src/app.js
+++ src/app.js
@@ -1,24 +1,186 @@
 const express = require("express");
 const connectDB = require("../config/dataBase.js");
 const User = require("./models/user.model.js");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.json());
 
-// this is post api to create new user
-app.post("/singup", (req, res) => {
-  // console.log(req.body)
-  const user = new User(req.body);
+// this is our sign up API to create a new user
+app.post("/singup", async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    emailId,
+    password,
+    age,
+    gender,
+    photoUrl,
+    about,
+    skills,
+  } = req.body;
 
-  user
-    .save()
-    .then(() => {
-      res.send("User saved succefully");
-    })
-    .catch((error) => {
-      res.send("somthing went wrong");
+  try {
+    // First Name
+    if (!firstName) {
+      throw new Error("First name is required");
+    }
+
+    if (typeof firstName !== "string") {
+      throw new Error("First name must be a string");
+    }
+
+    if (firstName.trim().length < 2 || firstName.trim().length > 50) {
+      throw new Error("First name must be between 2 and 50 characters");
+    }
+
+    // Last Name
+    if (!lastName) {
+      throw new Error("Last name is required");
+    }
+
+    if (typeof lastName !== "string") {
+      throw new Error("Last name must be a string");
+    }
+
+    if (lastName.trim().length < 2 || lastName.trim().length > 50) {
+      throw new Error("Last name must be between 2 and 50 characters");
+    }
+
+    // Email
+    if (!emailId) {
+      throw new Error("Email is required");
+    }
+
+    if (!validator.isEmail(emailId)) {
+      throw new Error("Invalid email address");
+    }
+
+    // Password
+    if (!password) {
+      throw new Error("Password is required");
+    }
+
+    if (typeof password !== "string") {
+      throw new Error("Password must be a string");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      throw new Error("Password is not strong enough");
+    }
+
+    // Age
+    if (age === undefined || age === null) {
+      throw new Error("Age is required");
+    }
+
+    if (typeof age !== "number") {
+      throw new Error("Age must be a number");
+    }
+
+    if (age < 18 || age > 100) {
+      throw new Error("Age must be between 18 and 100");
+    }
+
+    // Gender
+    if (!gender) {
+      throw new Error("Gender is required");
+    }
+
+    if (!["male", "female", "other"].includes(gender.toLowerCase())) {
+      throw new Error("Invalid gender");
+    }
+
+    // Photo URL
+    if (photoUrl && !validator.isURL(photoUrl)) {
+      throw new Error("Invalid photo URL");
+    }
+
+    // About
+    if (about) {
+      if (typeof about !== "string") {
+        throw new Error("About must be a string");
+      }
+
+      if (about.trim().length > 500) {
+        throw new Error("About cannot exceed 500 characters");
+      }
+    }
+
+    // Skills
+    if (skills) {
+      if (!Array.isArray(skills)) {
+        throw new Error("Skills must be an array");
+      }
+
+      if (skills.length > 20) {
+        throw new Error("Maximum 20 skills are allowed");
+      }
+    }
+
+    // hash password
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // Create User
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      emailId,
+      password: hashPassword,
+      age,
+      gender,
+      photoUrl,
+      about,
+      skills,
     });
+
+    res.status(201).send({
+      message: "Signup successful",
+      data: newUser,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+    });
+  }
+});
+
+// this is our loggin API to logging a user succefully
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    
+
+    const foundUser = await User.findOne({ emailId: emailId });
+    
+
+    if (!foundUser) {
+      throw new Error("Invelid Credentials");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      foundUser.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new Error("Password is not correct");
+    }
+
+    res.status(200).json({
+      msg: "User logged in succefully",
+      data: foundUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Something went wrong",
+      error: error.message,
+    });
+  }
 });
 
 // this api is to get the single user info
@@ -27,20 +189,52 @@ app.get("/user", async (req, res) => {
   const email = req.body.emailId;
 
   try {
-
-    const foundUser = await User.find({ emailId: email }); 
+    const foundUser = await User.find({ emailId: email });
     res.send({
       message: "user found succefully",
-      data: foundUser
-    })
-    
+      data: foundUser,
+    });
   } catch (error) {
-
     res.send("somthig went wrong");
-    
   }
+});
 
-  
+// this is feed api to get all the data of database
+
+app.get("/feed", async (req, res) => {
+  const foundData = await User.find({});
+
+  res.send({
+    msg: "Data fetch succefully",
+    data: foundData,
+  });
+});
+
+// thi api is for to update any specific data
+
+app.patch("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const newUser = req.body;
+
+  const updateUser = await User.findByIdAndUpdate(id, newUser);
+
+  res.send({
+    msg: "User Updated Succefully",
+    data: updateUser,
+  });
+});
+
+// this api is to delete a user
+
+app.delete("/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const findUser = await User.findByIdAndDelete(id);
+
+  res.send({
+    msg: "user deleted succefully",
+    data: findUser,
+  });
 });
 
 connectDB()
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,29 +1,94 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minlength: [2, "First name must be at least 2 characters"],
+      maxlength: [50, "First name cannot exceed 50 characters"],
+    },
+
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      minlength: [2, "Last name must be at least 2 characters"],
+      maxlength: [50, "Last name cannot exceed 50 characters"],
+    },
+
+    emailId: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      maxlength: [100, "Email cannot exceed 100 characters"],
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please enter a valid email address",
+      ],
+    
+    
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+     
+    },
+
+    age: {
+      type: Number,
+      min: [18, "Age must be at least 18"],
+      max: [100, "Age cannot exceed 100"],
+    },
+
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: "Invalid gender",
+      },
+      lowercase: true,
+      trim: true,
+    },
+
+    photoUrl: {
+      type: String,
+      trim: true,
+      maxlength: [500, "Photo URL is too long"],
+      match: [
+        /^https?:\/\/.+/,
+        "Photo URL must be a valid URL",
+      ],
+    },
+
+    about: {
+      type: String,
+      trim: true,
+      maxlength: [500, "About cannot exceed 500 characters"],
+    },
+
+    skills: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (skills) {
+          return skills.length <= 20;
+        },
+        message: "You can add maximum 20 skills",
+      },
+    },
   },
+  {
+    timestamps: true,
+    versionKey: false,
+    strict: true,
+  }
+);
 
-  lastName: {
-    type: String,
-  },
-
-  emailId: {
-    type: String,
-  },
-
-  password: {
-    type: String,
-  },
-
-  age: {
-    type: Number,
-  },
-
-  gender: {
-    type: String,
-  },
-});
-
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
